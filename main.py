aab5de16d1218e68b3fac971274426f86387847d
Trenton Sims <tsims115@yahoo.com>
2022-04-15 12:22:39 -0500

    Adding changes

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
 import pygame
 import pygame_assets as assets
+from sys import exit
 """Simple game with a goal to catch flowers and avoid bad stuff"""
 
 WIDTH = 800
 HEIGHT = 600
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 SKY_COLOR = (77, 204, 255)
+FPS = 60
+WINDOW.fill(SKY_COLOR)
+pygame.display.update()
 
-def draw_window():
-    WINDOW.fill(SKY_COLOR)
-    pygame.display.update()
 
 
 if __name__ == "__main__":
-    FPS = 60
     clock = pygame.time.Clock()
     running = True
     """Main game loop"""
     while running:
         clock.tick(60)
+
+        """Main event loop"""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
-        draw_window()
-    pygame.quit()
+                exit()
+        
